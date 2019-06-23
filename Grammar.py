a2d7ff2aee0f7361181809d12bb1a9f318cc3128
jviriato <josevviriato@gmail.com>
2019-06-22 22:06:25 -0300

    função bizarra add

--- Grammar.py
+++ Grammar.py
@@ -1,2 +1,48 @@
-class Grammar():
-    pass
+import re
+
+class Grammar:
+    def __init__(self, grammar_to_parse):
+        self.regex = self.doRegex(grammar_to_parse)
+        self.startSymbol = self.setStartSymbol()
+        self.terminalSymbols = self.setSymbols(self.regex.group(4))
+        self.nonTerminalSymbols = self.setSymbols(self.regex.group(2))
+        self.rules = self.setRules(self.regex.group(7))
+        print(self.rules)
+    def doRegex(self, grammar_to_parse):
+        pattern = r"(G\s*=\s*\()(\s*{\s*([A-Z]\s*,\s*)*[A-Z]+\s*})\s*,\s*({\s*([a-z]\s*,\s*)*[a-z]+\s*})\s*,\s*([A-Z]{1})\s*,\s*(\s*{\s*(([A-Z]\s*->\s*&\s*|\s*[A-Z]\s*->\s*[a-z]*\s*[A-Z]?)\s*,*)*\s*}\s*)(\))$"
+        search = re.search(pattern, grammar_to_parse)
+        return search
+
+    def printRegex(self):
+        print('Símbolos não terminais: ' + self.regex.group(2))
+        print('Símbolos terminais: ' + self.regex.group(4))
+        print('Símbolo inicial: ' + self.regex.group(6))
+        print('Transformações: ' + self.regex.group(7))
+
+    def setStartSymbol(self):
+        return self.regex.group(6)
+
+    def setSymbols(self, regex):
+        return re.sub('{|}| ', '', regex).split(',')
+
+    # Primeiro, remove os caracteres inúteis.
+    # Cada elemento separado por vírgula é colocado numa lista
+    # Então, separa o que está à esquerda de -> com o que está à direita.
+    # Transforma isto numa lista de listas.
+    # Por fim, transforma a lista interna em um dicionário
+    def setRules(self, regex):
+        return list(map(lambda a: dict(zip(a[::2], a[1::2])),
+               list(map(lambda s: s.split('->'),re.sub('{|}| ', '', regex)
+               .split(',')))))
+
+    def validateGrammar(self):
+        try: 
+            validation = self.validateStartSymbol()
+            if validation is False:
+                raise ValueError('Start Symbol not in Non-Terminal Symbols')
+        except ValueError as e:
+            exit(str(e))
+
+
+    def validateStartSymbol(self):
+        return self.startSymbol in self.nonTerminalSymbols
--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 import argparse
-import re
-def doRegex(grammar):
-    pattern = r"(G\s*=\s*\()(\s*{\s*([A-Z]\s*,\s*)*[A-Z]+\s*})\s*,\s*({\s*([a-z]\s*,\s*)*[a-z]+\s*})\s*,\s*([A-Z]{1})\s*,\s*(\s*{\s*(([A-Z]\s*->\s*&\s*|\s*[A-Z]\s*->\s*[a-z]*\s*[A-Z]?)\s*,*)*\s*}\s*)(\))$"
-    search = re.search(pattern, grammar)
-    print('Símbolos não terminais: ' + search.group(2))
-    print('Símbolos terminais: ' + search.group(4))
-    print('Símbolo inicial: ' + search.group(6))
-    print('Transformações: ' + search.group(7))
+from Grammar import Grammar
 def main():
     # parser = argparse.ArgumentParser(description='Process a Regular Grammar.')   
     # parser.add_argument('grammar', help='The grammar. Example: "" ') 
     # args = parser.parse_args()
     # grammar = args.grammar
-    grammar = "G = ( {S , A}, {a, b, c}, S, {S->aS,S->bA,A->&,A->cA})"
-    doRegex(grammar)
+    grammar = "G = ( {S , A}, {a, b, c}, V, {S->aS,S->bA,A->&,A->cA})"
+    g = Grammar(grammar)
+    g.printRegex()
+    g.validateGrammar()
 if __name__ == "__main__":
     main()
