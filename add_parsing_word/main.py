7ed9e8417c101c5b300993e22ac85b7b4185f51b
MzGrisotti <mzgrisotti@inf.ufsm.br>
2019-06-25 02:03:30 -0300

    Create main.py

---
+++ add_parsing_word/main.py
@@ -0,0 +1,14 @@
+import argparse
+from Grammar import Grammar
+def main():
+    grammar_path = 'gramatica_exemplo_4.txt'
+    with open(grammar_path, 'r') as gf:
+        grammar = gf.readline().rstrip()
+    g = Grammar(grammar)
+    g.validateGrammar()
+    # word = i
+    # nput('Digite a palavra a ser validada: ')
+    word = "aabbbb"
+    g.recognize(word)
+if __name__ == "__main__":
+    main()
